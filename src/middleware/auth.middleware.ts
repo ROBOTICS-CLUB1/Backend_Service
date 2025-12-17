62aa101f4d4bc7ee181f838bca3c4d13d7a5f5b7
Louis2324 <miguellouis2023@gmail.com>
2025-12-17 15:34:54 +0200

    Refactor: Improve formatting and readability of authMiddleware function

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,15 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JwtPayloadCustom } from "../types/jwtPayload";
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ message: "No token, authorization denied" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as JwtPayloadCustom;
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(401).json({ message: 'Token is not valid' });
+  } catch {
+    return res.status(401).json({ message: "Token is not valid" });
   }
 };
