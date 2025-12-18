94652d38a24f9353a411b291481d30e31624e1c2
Louis2324 <miguellouis2023@gmail.com>
2025-12-18 06:07:07 +0200

    Add: Implement validation for registration and login routes, and enhance post creation and update routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller";
+import {
+  registerValidator,
+  loginValidator,
+} from "../validators/auth.validator";
+import { validate } from "../middleware/validate.middleware";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerValidator, validate, register);
+router.post("/login", loginValidator, validate, login);
 
 export default router;
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -8,16 +8,21 @@ import {
 } from "../controllers/post.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { adminMiddleware } from "../middleware/role.middleware";
+import {
+  createPostValidator,
+  updatePostValidator,
+} from "../validators/post.validator";
+import { validate } from "../middleware/validate.middleware";
 
 const router = Router();
 
 router.use(authMiddleware);
 
 router.get("/", getPosts);
-router.get("/:id",getPost);
+router.get("/:id", getPost);
 
-router.post("/", adminMiddleware, createPost);
-router.put("/:id", adminMiddleware, updatePost);
+router.post("/", adminMiddleware, createPostValidator, validate, createPost);
+router.put("/:id", adminMiddleware, updatePostValidator, validate, updatePost);
 router.delete("/:id", adminMiddleware, deletePost);
 
 export default router;
