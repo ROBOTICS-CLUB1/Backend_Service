ede83350e6cab315072d037fc90db7866bf86fff
Louis2324 <miguellouis2023@gmail.com>
2025-12-22 11:03:21 +0200

    Added the validate parent type for comments

--- src/validators/comment.validator.ts
+++ src/validators/comment.validator.ts
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import { Request, Response, NextFunction } from "express";
 
 export const commentValidator = [
   body("content")
@@ -7,3 +8,16 @@ export const commentValidator = [
     .isLength({ max: 500 })
     .withMessage("Content must be 500 characters or less"),
 ];
+
+
+export const validateParentType = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { parentType } = req.params;
+  if (!["posts", "projects"].includes(parentType)) {
+    return res.status(400).json({ message: "Invalid parent type" });
+  }
+  next();
+};
